ac7cc19deba995ce2c538be6077a9e36016cd367
Ben Nuttall <ben@bennuttall.com>
2013-03-31 02:13:25 +0100

    Removed name attribute from submit button and removed unused CSS classes

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <input type="text" class="field" name="s" placeholder="Search..." />
-    <input type="submit" class="submit" name="submit" id="searchsubmit" value="Go" />
+    <input type="text" name="s" placeholder="Search..." />
+    <input type="submit" value="Go" />
 </form>
