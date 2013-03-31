85f3d4a9c98ac9e3d84ceca15b2f9a16c9429ac2
Ben Nuttall <ben@bennuttall.com>
2013-03-31 23:24:50 +0100

    Forced footer even if empty stopping floating diggdigg bar from breaking the page

--- post.php
+++ post.php
@@ -6,11 +6,11 @@
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     </header>
     <?php the_content(); ?>
-    <?php if (!is_page()): ?>
     <footer>
+    <?php if (!is_page()): ?>
         <?php show_tag_list(get_the_ID(), ', ', 'Tags: '); ?>
-    </footer>
     <?php endif; ?>
+    </footer>
 </article>
 <?php if (is_singular('post')): ?>
 <aside id="prevnext">
