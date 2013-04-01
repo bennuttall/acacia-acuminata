e65eccadeb0c8a7f8269280f516dcb88df890fbf
Ben Nuttall <ben@bennuttall.com>
2013-04-01 14:38:42 +0100

    Corrected wording of footer

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 <footer id="footer">
     <aside>
         <p><a href="https://github.com/bennuttall/Acacia-Acuminata"><em>Acacia Acuminata</em></a> Wordpress Theme by <a href="http://bennuttall.com/">Ben Nuttall</a> - source available on <a href="https://github.com/bennuttall/Acacia-Acuminata">github</a>.</p>
-        <p>Manchester Raspberry Jam part of the <a href="http://www.raspberryjam.org/">Raspberry Jam</a> network. We are sponsored and supported by <a href="http://www.bytemark.co.uk/r/mcrraspjam" id="bytemark">Bytemark Hosting</a></p>
+        <p>Manchester Raspberry Jam is part of the <a href="http://www.raspberryjam.org/">Raspberry Jam</a> network. We are sponsored and supported by <a href="http://www.bytemark.co.uk/r/mcrraspjam" id="bytemark">Bytemark Hosting</a></p>
     </aside>
 </footer>
 <?php wp_footer(); ?>
