d6edaaeb50befd0feebd397cedf98657defc044c
Ben Nuttall <ben@bennuttall.com>
2013-04-01 02:13:34 +0100

    Renamed theme to 'Acacia Acuminata' in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 </div>
+<aside id="twitter-badge">
+<a href="https://twitter.com/McrRaspJam" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @McrRaspJam</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</aside>
 <footer id="footer">
     <aside>
-        <p>Wordpress Theme by <a href="http://bennuttall.com/">Ben Nuttall</a> - source available on <a href="https://github.com/bennuttall/Jam-Wordpress-Theme">github</a>.</p>
+        <p><em>Acacia Acuminata</em> Wordpress Theme by <a href="http://bennuttall.com/">Ben Nuttall</a> - source available on <a href="https://github.com/bennuttall/Acacia-Acuminata">github</a>.</p>
         <p>Manchester Raspberry Jam part of the <a href="http://www.raspberryjam.org/">Raspberry Jam</a> network. We are sponsored and supported by</p>
     </aside>
     <aside class="sponsor">
