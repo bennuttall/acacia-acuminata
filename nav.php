63c7c2aa33a713f6141fec575eb50adfa796bdd9
Ben Nuttall <ben@bennuttall.com>
2013-07-11 01:08:10 +0100

    Unindented

--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <nav id="nav">
     <div class="nav-holder">
-    <?php get_search_form(); ?>
-    <?php wp_nav_menu(array('theme_location' => 'nav_bar', 'container' => false)); ?>
-    <div class="break">&nbsp;</div>
+        <?php get_search_form(); ?>
+        <?php wp_nav_menu(array('theme_location' => 'nav_bar', 'container' => false)); ?>
+        <div class="break">&nbsp;</div>
     </div>
 </nav>
