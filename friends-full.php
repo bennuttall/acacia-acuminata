74a6a335baf40a955b933e442708d0f4ed28b0a0
Ben Nuttall <ben@bennuttall.com>
2013-11-08 21:56:14 +0000

    Show two friends on each row, remove title

--- friends-full.php
+++ friends-full.php
@@ -3,22 +3,28 @@
 $args = array(
     'post_type' => 'friends',
     'orderby' => 'rand',
-    'posts_per_page' => 30,
+    'posts_per_page' => 100,
 );
 
 $friends = new WP_Query($args);
 
+$i = 0;
+
 ?>
-<aside id="friends">
-<h2>Friends of Manchester Raspberry Jam</h2>
-<div class="logos">
-<?php while ($friends->have_posts()):
-    $friends->the_post();
-    $thumbnail_args = array('title' => get_the_title());
-?>
-<h3><?php the_title(); ?></h3>
-<a href="<?php the_field('url'); ?>" target="_blank"><?php the_post_thumbnail('friend_thumb', $thumbnail_args); ?></a>
-<?php the_excerpt(); ?>
-<?php endwhile; ?>
-</div>
-</aside>
+<section id="friends">
+    <h2>Friends of Manchester Raspberry Jam</h2>
+    <?php while ($friends->have_posts()):
+        $friends->the_post();
+        $thumbnail_args = array('title' => get_the_title());
+    ?>
+    <div class="friend">
+        <a href="<?php the_field('url'); ?>" target="_blank"><?php the_post_thumbnail('friend_thumb', $thumbnail_args); ?></a>
+        <?php the_excerpt(); ?>
+    </div>
+    <?php if ($i % 2): ?>
+        <div style="clear:both;"></div>
+    <?php endif;
+        $i++;
+    endwhile; ?>
+    <div style="clear:both;"></div>
+</section>
