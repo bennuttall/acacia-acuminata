46eae4e73120abe57dffd7a04454ab3c52a4f275
Ben Nuttall <ben@bennuttall.com>
2013-12-18 14:39:28 +0000

    Add Google+ link to head as author

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <link rel="publisher" href="https://plus.google.com/+McrRaspJamOrgUk?rel=publisher" />
+    <link rel="author" href="https://plus.google.com/+McrRaspJamOrgUk?rel=author" />
     <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
     <!--[if lt IE 9]>
     <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
